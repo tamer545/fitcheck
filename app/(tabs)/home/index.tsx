758a708f102f713b349d0284d8ac6ee5f5859bc2
Tamer545 <8235.Lohn>
2023-07-13 10:53:17 +0200

    feat: add stopwatch

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 import firebase from "firebase/compat";
 import {Link, Redirect} from "expo-router";
 import * as Location from "expo-location";
 import {LocationAccuracy, LocationGeocodedAddress, LocationObjectCoords, LocationSubscription} from "expo-location";
 import Spinner from "react-native-loading-spinner-overlay";
 import MapView, {Marker} from "react-native-maps";
+
+// @ts-ignore
+import { Stopwatch } from 'react-native-stopwatch-timer';
+
 
 
 export default function App() {
@@ -21,6 +25,8 @@ export default function App() {
     });
     const [training, setTraining] = useState(false)
     const [running, setRunning] = useState(false)
+    const [isStopwatchStart, setIsStopwatchStart] = useState(false);
+    const [resetStopwatch, setResetStopwatch] = useState(false);
 
     useEffect(() => {
         (() => {
@@ -32,8 +38,8 @@ export default function App() {
                 setCurrentLocation(coords);
                 setMapRegion(() => {
                     return {
-                        latitude: coords.latitude, longitude: coords.longitude, latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
+                        latitude: coords.latitude, longitude: coords.longitude, latitudeDelta: 0.01,
+                        longitudeDelta: 0.01,
                     }
                 })
             }).then((locationWatcher) => {
@@ -58,10 +64,9 @@ export default function App() {
         })();
     }, []);
 
-    if (training){
+    if (training) {
 
-    }
-    else if (running){
+    } else if (running) {
         return (
             <SafeAreaView>
                 {loading ? (
@@ -73,13 +78,46 @@ export default function App() {
                         />
                     </View>
                 ) : (
-                <MapView
+                    <View>
+                        <MapView
                             style={{height: 300}}
                             region={mapRegion}
                         >
-                            <Marker coordinate={mapRegion} title='Marker'/>
                         </MapView>
-                    )}
+                        <Button title={"Lauf beenden"} onPress={() => setRunning(false)}/>
+                        <View style={styles.sectionStyle}>
+                            <Stopwatch
+                                laps
+                                msecs
+                                start={isStopwatchStart}
+                                //To start
+                                reset={resetStopwatch}
+                                //To reset
+                                options={options}
+                                //options for the styling
+                                getTime={(time: any) => {
+                                    console.log(time);
+                                }}
+                            />
+                            <TouchableHighlight
+                                onPress={() => {
+                                    setIsStopwatchStart(!isStopwatchStart);
+                                    setResetStopwatch(false);
+                                }}>
+                                <Text style={styles.buttonText}>
+                                    {!isStopwatchStart ? 'START' : 'STOP'}
+                                </Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight
+                                onPress={() => {
+                                    setIsStopwatchStart(false);
+                                    setResetStopwatch(true);
+                                }}>
+                                <Text style={styles.buttonText}>RESET</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                )}
             </SafeAreaView>
         )
     }
@@ -87,12 +125,12 @@ export default function App() {
     if (!errorMsg) {
         return (
             <SafeAreaView style={styles.container}>
-                    <View>
-                        <Text style={styles.paragraph}>Training</Text>
-                        <Button title={"Training Starten"}/>
-                        <Text style={styles.paragraph}>Lauf</Text>
-                        <Button title={"Lauf Starten"} onPress={() => setRunning(true)}/>
-                    </View>
+                <View>
+                    <Text style={styles.paragraph}>Training</Text>
+                    <Button title={"Training Starten"}/>
+                    <Text style={styles.paragraph}>Lauf</Text>
+                    <Button title={"Lauf starten"} onPress={() => setRunning(true)}/>
+                </View>
             </SafeAreaView>
 
         )
@@ -101,21 +139,46 @@ export default function App() {
 
 }
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            alignItems: 'center',
-            padding: 20,
-        },
-        paragraph: {
-            fontSize: 20,
-            marginTop: 5,
-            textAlign: "center"
-        },
-        heading: {
-            marginTop: 20
-        },
-        spinnerTextStyle: {
-            color: '#FFF',
-        },
-    });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 20,
+    },
+    paragraph: {
+        fontSize: 20,
+        marginTop: 5,
+        textAlign: "center"
+    },
+    heading: {
+        marginTop: 20
+    },
+    spinnerTextStyle: {
+        color: '#FFF',
+    },
+    sectionStyle: {
+        flex: 1,
+        marginTop: 32,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontSize: 20,
+        marginTop: 10,
+    },
+});
+
+const options = {
+    container: {
+        backgroundColor: '#FF0000',
+        padding: 5,
+        borderRadius: 5,
+        width: 200,
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 25,
+        color: '#FFF',
+        marginLeft: 7,
+    },
+};
